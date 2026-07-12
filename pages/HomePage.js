0a70b1ef09cfdb796c221ab97cb12062e767380b
sridevi merla <sridevi678sri@gmail.com>
2026-07-12 14:17:43 +0530

    modified my accoutn to fial etsts to see if merge button disables

--- pages/HomePage.js
+++ pages/HomePage.js
@@ -4,7 +4,7 @@ export default class HomePage
    constructor(page)
     {
        
-        this.myAccount=page.getByRole("link",{name:'My Account'}).first();
+        this.myAccount=page.getByRole("link",{name:'My ccount'}).first();
         this.register=page.getByRole("link",{name:'Register'});
         this.login=page.getByRole("link",{name:'Login'});
         
