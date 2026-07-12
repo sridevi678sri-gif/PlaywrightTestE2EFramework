760416ea9120cdf41c68feb1da63a8fcab16f99b
sridevi merla <sridevi678sri@gmail.com>
2026-07-12 14:07:19 +0530

    added comment in sridevi branch

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,7 @@
 import {test,expect} from '@playwright/test';
 import POMManager from '../pages/POMManager';
 test("login",async({browser})=>{
-  
+  //login -sridevi
     const context= await browser.newContext();
     const page=await context.newPage();
     const pomManager=new POMManager(page);
